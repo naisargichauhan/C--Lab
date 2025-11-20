421c1358e64091f41f1ffffa5042a4badccb5b38
Naisargi Chauhan <naisargichauhan2@gmail.com>
2025-11-20 20:06:26 +0530

    Add files via upload

---
+++ Codeblock3.18.c
@@ -0,0 +1,25 @@
+//
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 2; i++) {
+        for(j = 1; j <= 3; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+
+    return 0;
+}
+int main() {
+    int i, j;
+
+    for(i = 1; i <= 3; i++) {
+        for(j = 1; j <= 2; j++) {
+            printf("%d %d\n", i, j);
+        }
+    }
+
+    return 0;
+}
